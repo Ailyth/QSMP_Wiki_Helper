f62bf55f58595393672630030f1ddc2d55802fa5
Ailyth <ailythpoe@gmail.com>
2026-09-17 21:32:00 +0200

    Use service account instead of oauth

--- data_loader.py
+++ data_loader.py
@@ -1,37 +1,20 @@
 import gspread
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
-from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 from vod_parser import is_month_sheet
 import os
 
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SERVICE_ACCOUNT_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "service-account.json"
+)
 
 def authorize():
-    # Open OAuth in Firefox private window
-    os.environ["BROWSER"] = "firefox --private-window"
-
-    creds = None
-
-    # Load token.json if it exists
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-    # If no valid token, do OAuth login
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-
-        # Save token.json
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
+    creds = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_PATH,
+        scopes=SCOPES
+    )
     return gspread.authorize(creds)
 
 def load_sheet(gc, url):
